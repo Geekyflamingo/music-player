47ed351f300d0cc22ee33b66f1f92b380cfd338a
Becca <rebecca.vasil@gmail.com>
2015-01-16 16:41:49 -0700

    got formatting looking better

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-"use strict";
+$.getJSON('data.json', function(tracks){
+  var $tracksTemplate = $('.tracksTemplate').html();
+  var newHTML = Mustache.to_html($tracksTemplate, tracks);
 
-$('.container').on('click', '.fa-play', function() {
-  $('.fa-stop').addClass('fa-play').removeClass('fa-stop');
-  $(this).removeClass('fa-play').addClass('fa-stop');
+  $('.tracks').html(newHTML);
 });
 
+  $('.container').on('click', '.fa-play', function(){
+    $('i').removeClass('fa-stop');
+    $('i').addClass('fa-play');
+    $(this).removeClass('fa-play');
+    $(this).addClass('fa-stop');
+    $('h1').text("Now playing: "  );
+    document.getElementById("thrill-of-the-hunt").play();
 
-$('.container').on('click', '.fa-stop', function() {
-  $(this).removeClass('fa-stop').addClass('fa-play');
-});
+  });
+  $('.container').on('click', '.fa-stop', function(){
+    $(this).removeClass('fa-stop');
+    $(this).addClass('fa-play');
+    $('h1').text( "Select a Song!" );
+  });
